d4ae47acde86d947c4aded2a400b943c6f627332
SDS <SDS@DESKTOP-K14Q8U2>
2021-10-20 22:09:54 +0900

    hn_03 merge

--- DuckBang/src/main/webapp/WEB-INF/views/map/elements/map.jsp
+++ DuckBang/src/main/webapp/WEB-INF/views/map/elements/map.jsp
@@ -4,29 +4,21 @@
   <head>
     <title>Places Search Box</title>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath }/resources/assets/css/map/map.css" />
+    <link rel="stylesheet" type="text/css" href="<c:url value='/resources/assets/css/map/map.css'/>" />
   </head>
 	<body>
 	
-	<div class= "map_searchBar">
-		<div class = "map_searchWrap">
-			<div class = "map_selectWrap">
-				<div class = "map_inputLiner">
-					<div class = "map_input-wrap">
-    					<input type="text" placeholder="지역, 지하철역, 학교 검색"/>
-    				</div>
-    				<div class="map_search-wrap">
-    					<button id="map_btn" class="map_search-btn"></button>
-    				</div>
-    			</div>
-    		</div>
+	<div id="map_map">
+    	<input id="pac-input" class="controls" type="text" placeholder="지역, 지하철역, 학교 검색"/>
+    	<div class="map_btn_div">
+    		<button id="map_btn"></button>
     	</div>
     </div>
     
     <div id="map"></div>
 
 	<script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCO1eRP2ExsPqAhGnAd2H6FO2Zo2dIefYE&callback=initAutocomplete&libraries=places&v=weekly&libraries=places&region=kr"
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCO1eRP2ExsPqAhGnAd2H6FO2Zo2dIefYE&callback=initMap&libraries=places&v=weekly&region=kr"
       async
     ></script>
     <script src="${pageContext.request.contextPath }/resources/assets/js/map/mainMap.js"></script>
--- DuckBang/src/main/webapp/resources/assets/js/map/mainMap.js
+++ DuckBang/src/main/webapp/resources/assets/js/map/mainMap.js
@@ -1,124 +1,197 @@
-function initAutocomplete() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: 37.566826,
-      lng: 126.9786567
-    },
-    zoom: 18,
-    mapTypeControl: false,
-    fullscreenControl: false,
-    mapTypeId: "roadmap"
-  });
+var cityCircle;
+var infoWindow;
+
+function initMap() {
+	const map = new google.maps.Map(document.getElementById("map"), {
+		center: {
+			lat: 37.566826,
+			lng: 126.9786567
+		},
+		zoom: 18,
+		mapTypeControl: false,
+		fullscreenControl: false,
+		mapTypeId: "roadmap"
+	});
   
-  // Create the search box and link it to the UI element.
-  const input = document.getElementById("pac-input");
-  const map_map = document.getElementById("map_map");
-  const input_div = input.parentNode;
-  const map_btn = document.getElementById("map_btn");
-  const searchBox = new google.maps.places.SearchBox(input);
+	// Create the search box and link it to the UI element.
+	const input = document.getElementById("pac-input");
+	const map_map = document.getElementById("map_map");
+	const input_div = input.parentNode;
+	const map_btn = document.getElementById("map_btn");
+	const searchBox = new google.maps.places.SearchBox(input);
 
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(map_map);
-  input.style.marginTop = "13px";
-  input.style.marginLeft = "13px";
+	map.controls[google.maps.ControlPosition.TOP_LEFT].push(map_map);
+	input.style.marginTop = "13px";
+	input.style.marginLeft = "13px";
   
-  // Bias the SearchBox results towards current map's viewport.
-  const clickEvent = map.addListener("bounds_changed", () => {
-    					searchBox.setBounds(map.getBounds());
-  					});
+	// Bias the SearchBox results towards current map's viewport.
+	const clickEvent = map.addListener("bounds_changed", () => {
+							searchBox.setBounds(map.getBounds());
+						});
 
-  let markers = [];
+	let markers = [];
 
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener("places_changed", () => {
-    const places = searchBox.getPlaces();
+	// Listen for the event fired when the user selects a prediction and retrieve
+	// more details for that place.
+	searchBox.addListener("places_changed", () => {
+		const places = searchBox.getPlaces();
 
-    if (places.length == 0) {
-      return;
-    }
+		if (places.length == 0) {
+			return;
+		}
 
-    // Clear out the old markers.
-    markers.forEach((marker) => {
-      marker.setMap(null);
-    });
-    markers = [];
+		// Clear out the old markers.
+		markers.forEach((marker) => {
+			marker.setMap(null);
+		});
+		markers = [];
 
-    // For each place, get the icon, name and location.
-    const bounds = new google.maps.LatLngBounds();
+		// For each place, get the icon, name and location.
+		const bounds = new google.maps.LatLngBounds();
 
-    places.forEach((place) => {
-      if (!place.geometry || !place.geometry.location) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
+		places.forEach((place) => {
+			if (!place.geometry || !place.geometry.location) {
+				console.log("Returned place contains no geometry");
+				return;
+			}
 
-      const icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25),
-      };
+			const icon = {
+				url: place.icon,
+				size: new google.maps.Size(71, 71),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(17, 34),
+				scaledSize: new google.maps.Size(25, 25),
+			};
 
-      // Create a marker for each place.
-      markers.push(
-        new google.maps.Marker({
-          map,
-          icon,
-          title: place.name,
-          position: place.geometry.location,
-        })
-      );
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
+			// Create a marker for each place.
+			markers.push(
+				new google.maps.Marker({
+					map,
+					icon,
+					title: place.name,
+					position: place.geometry.location,
+				})
+			);
+			
+			if (place.geometry.viewport) {
+				// Only geocodes have viewport.
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+			
+			// 검색위치 반경에 원 표시해주기
+			var latitude = place.geometry.location.lat();	// 위도
+			var longitude = place.geometry.location.lng();	// 경도
+
+			const citymap = {
+				center: { lat: latitude, lng: longitude},
+			};
+			
+			// 여러개 나오게하는거 방지
+			if(cityCircle) {
+				cityCircle.setMap(null);
+			}
+			
+			// 지도 드래그할때 원이랑 마커 지워주기
+			google.maps.event.addListener(map, 'drag', () => {
+				cityCircle.setMap(null);
+				for(var idx = 0; idx < markers.length; idx++) {
+				    markers[idx].setMap(null);
+				}
+			});
+
+			// Add the circle for this city to the map.
+			// 맨위에 var cityCircle선언해줌
+			cityCircle = new google.maps.Circle({
+				strokeColor: "gray",	// 선의 색
+				strokeOpacity: 0.8,		// 선의 불투명도
+				strokeWeight: 2,		// 선의 두께
+				fillColor: "gray",		// 채우기 색깔
+				fillOpacity: 0.35,		// 채우기 불투명도
+				map,					// 표시할 맵의 이름
+				center: citymap.center,	// 원의 중심좌표
+				radius: 500,			// 원의 반지름 (단위: 미터)
+			});
+      		// 여기까지 검색위치 반경에 원표시 해주기
+		});
+		map.fitBounds(bounds);
+	});
+  
+	// 현재 위치 불러오기
+	infoWindow = new google.maps.InfoWindow();
+
+	const locationButton = document.createElement("button");
+
+	locationButton.classList.add("custom-map-control-button");
+	map.controls[google.maps.ControlPosition.TOP_RIGHT].push(locationButton);
+	locationButton.addEventListener("click", () => {
+		// Try HTML5 geolocation.
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				const pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude,
+				};
+
+			const image = new google.maps.MarkerImage("https://thumb.ac-illust.com/t/64/64e74d485b6852392365e01e969678fe_t.jpeg", null, null, null, new google.maps.Size(20,20));
+			
+			// 현재 위치에 마커 표시해주기
+			new google.maps.Marker({
+			    position: pos,
+			    map,
+			    title: "현재 위치", // 마커에 mouseover했을때 나오는 글
+			    icon: image,
+			  });
+			
+			//infoWindow.setPosition(pos);
+			//infoWindow.setContent("<div style='background-color: black;'>현재위치</div>");
+			infoWindow.open(map);
+			map.setCenter(pos);
+		},() => {
+			handleLocationError(true, infoWindow, map.getCenter());
+			});
+		} else {
+			// Browser doesn't support Geolocation
+			handleLocationError(false, infoWindow, map.getCenter());
+		}
+	});
+	// 여기까지 현재 위치 불러오기
+	
+	//여러개 마커 찍기
+	makeMarkers(map);
 }
 
-infoWindow = new google.maps.InfoWindow();
+// 여러가지 마커들 찍기
+function makeMarkers(map) {
+	var locations = [
+		['서울역', 37.5546788, 126.9706069],
+		['서울특별시청', 37.5668260, 126.9786567],
+		['을지로입구역', 37.5660373, 126.9821930],
+		['덕수궁', 37.5655638, 126.974894],
+	];
+	
+	
+	var i, marker;
+	
+	for(i = 0; i < locations.length; i++){
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			label: "1",
+			map: map,
+		});
+		
+	}
+	//console.log($('#map').html(marker.length));
+}
 
-const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
-
+// 현재위치 검색 시 에러 잡아주는 함수
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(
+		browserHasGeolocation
+		? "Error: The Geolocation service failed."
+		: "Error: Your browser doesn't support geolocation."
+	);
+	infoWindow.open(map);
 }
-

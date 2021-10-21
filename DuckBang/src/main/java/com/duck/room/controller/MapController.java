311e256b3ef95db1b217588f23f5ac4569e14a32
leehannah <tngus1085@naver.com>
2021-10-21 21:20:41 +0900

    HN_04 10-21 commit

---
+++ DuckBang/src/main/java/com/duck/room/controller/MapController.java
@@ -0,0 +1,43 @@
+package com.duck.room.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.duck.room.mapper.MapMapper;
+
+@Controller
+public class MapController {
+
+	@Autowired
+	MapMapper mapMapper;
+	
+	@GetMapping("/map")
+	public String mapMarkers(Model model) {
+		
+		ArrayList<Integer> o_id = new ArrayList<>();
+		ArrayList<String> xcordinate = new ArrayList<>();
+		ArrayList<String> ycordinate = new ArrayList<>();
+		
+		for(int i = 0; i < mapMapper.getList().size(); ++i) {
+			o_id.add(mapMapper.getList().get(i).getO_id());
+			xcordinate.add(mapMapper.getList().get(i).getL_xcordinate());
+			ycordinate.add(mapMapper.getList().get(i).getL_ycordinate());
+		}
+		
+		model.addAttribute("o_id", o_id);
+		model.addAttribute("xcordinate", xcordinate);
+		model.addAttribute("ycordinate", ycordinate);
+		
+		return "map/index";
+	}
+	
+	@GetMapping("/oIdInfo")
+	public String o_id_info(Integer o_id) {
+		System.out.println("매물번호 : " + o_id);
+		return "info/index";
+	}
+}
--- DuckBang/src/main/webapp/WEB-INF/views/map/elements/map.jsp
+++ DuckBang/src/main/webapp/WEB-INF/views/map/elements/map.jsp
@@ -10,13 +10,16 @@
 	
 	<div id="map_map">
     	<input id="pac-input" class="controls" type="text" placeholder="지역, 지하철역, 학교 검색"/>
+    	<input id="value_o_id" type="hidden" value="${o_id }"/>
+    	<input id="value_xcordinate" type="hidden" value="${xcordinate }"/>
+    	<input id="value_ycordinate" type="hidden" value="${ycordinate }"/>
     	<div class="map_btn_div">
     		<button id="map_btn"></button>
     	</div>
     </div>
     
     <div id="map"></div>
-
+	
 	<script
       src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCO1eRP2ExsPqAhGnAd2H6FO2Zo2dIefYE&callback=initMap&libraries=places&v=weekly&region=kr"
       async
